889e6a54cb8756895bed20329cedc5fde02a1967
Jhana Gustave <jhana.gustave@gmail.com>
2019-05-08 20:56:34 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,72 +1,65 @@
 class Owner
-  # code goes here
+  @@all= []
+  attr_accessor :name, :pets
   attr_reader :species
-  attr_accessor :name, :pets, :fish, :dog, :cat
 
-  @@all = []
-  @@pets = {:fishes => [], :dogs => [], :cats => []}
+  # Instance Methods #
 
   def initialize(species)
     @species = species
-    @name = name
     @@all << self
-  end
-
-  def self.all
-    @@all
-  end
-
-  def self.count
-    @@all.length
-  end
-
-  def self.reset_all
-    @@all.clear
+    @pets = {:fishes => [], :dogs => [], :cats => []}
   end
 
   def say_species
-    "I am a #{@species}."
+    return "I am a #{@species}."
   end
 
-  def pets
-    @@pets
+  # Pets #
+
+  def buy_fish(name_of_fish)
+    @pets[:fishes] << Fish.new(name_of_fish)
   end
 
-  def buy_fish(fish)
-    @@pets[:fishes] << Fish.new(fish)
+  def buy_dog(name_of_dog)
+    @pets[:dogs] << Dog.new(name_of_dog)
   end
 
-  def buy_cat(cat)
-    @@pets[:cats] << Cat.new(cat)
-  end
-
-  def buy_dog(dog)
-    @@pets[:dogs] << Dog.new(dog)
+  def buy_cat(name_of_cat)
+    @pets[:cats] << Cat.new(name_of_cat)
   end
 
   def walk_dogs
-    @@pets[:dogs].each do |dog|
-      dog.mood = 'happy'
+    @pets.collect do |species, instances|
+      if species == :dogs
+        instances.each do |dog|
+          dog.mood = "happy"
+        end
+      end
     end
   end
 
   def play_with_cats
-    @@pets[:cats].each do |cat|
-      cat.mood = 'happy'
+    @pets.collect do |species, instances|
+      if species == :cats
+        instances.each do |cat|
+          cat.mood = "happy"
+        end
+      end
     end
   end
 
   def feed_fish
-    @@pets[:fishes].each do |fish|
-      fish.mood = 'happy'
+    @pets.collect do |species, instances|
+      if species == :fishes
+        instances.each do |fish|
+          fish.mood = "happy"
+        end
+      end
     end
   end
 
-  def list_pets
-    "I have #{pets[:fishes].length} fish, #{pets[:dogs].length} dog(s), and #{pets[:cats].length} cat(s)."
-  end
-
- def sell_pets
+  def sell_pets
     @pets.collect do |species, instances|
       instances.each do |pet|
         pet.mood = "nervous"
@@ -75,4 +68,9 @@ class Owner
     end
   end
 
-end
+  def list_pets
+    num_dogs = @pets[:dogs].size
+    num_cats = @pets[:cats].size
+    num_fish = @pets[:fishes].size
+    return "I have #{num_fish} fish, #{num_dogs} dog(s), and #{num_cats} cat(s)."
+  end
