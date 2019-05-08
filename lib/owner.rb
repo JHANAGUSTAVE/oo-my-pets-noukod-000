7fe974cb0f32586d938e4dd7bcb75a391fad9e99
Jhana Gustave <jhana.gustave@gmail.com>
2019-05-08 20:53:19 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -69,7 +69,8 @@ class Owner
   def sell_pets
     pets.each do |pet, arr|
       arr.map do |pet|
-        pet.mood = 'nervous"
+        pet.mood = "nervous"
+        
       end
       arr.clear
     end
